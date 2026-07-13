196fcc3ddf662327508a713c1abb4eb0d0a5eb20
Sergio Florean Chavez <sergio_floreanch@live.com>
2026-07-13 13:04:38 -0600

    add first react

---
+++ first_react/script.jsx
@@ -0,0 +1,12 @@
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <div>
+    <div className="item" style={{ borderRadius: "0%" }}>
+      Cuadrado
+    </div>
+    <div className="item" style={{ borderRadius: "50%" }}>
+      Círculo
+    </div>
+  </div>,
+);
